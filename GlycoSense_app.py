9244f35ee03a6e20a8554f07c92c251ce59c9c7e
hamsis-techy <hamdaansayyed007@gmail.com>
2026-06-20 01:41:33 +0300

    Initial commit

---
+++ GlycoSense_app.py
@@ -0,0 +1,42 @@
+import streamlit as st
+import numpy as np 
+import pickle as pkl
+
+ 
+
+# load the GlycoSense Model
+Glycosense_model = pkl.load('GlycoSense_model.pkl','rb')
+
+def glyco_predict(inputs):
+  
+  # changing the input_data to numpy array
+  inputs_arr = np.asarray(inputs).reshape(1,-1)
+
+  prediction = Glycosense_model.predict(inputs_arr)
+  print(prediction)
+
+  if (prediction[0] == 0):
+    return('The person is not diabetic')
+  else:
+    return('The person is diabetic')
+
+def main():
+    st.header('GlycoSense - Diabeties Predictor')
+
+    Pregnancies = st.text_input('Number of Pregnancies')
+    Glucose	    = st.text_input('Glucose level')
+    BloodPressure	 = st.text_input('BP level')
+    SkinThickness	= = st.text_input('SkinThickness value')
+    Insulin	= st.text_input('Insulin level')
+    BMI	= st.text_input('BMI')
+    DiabetesPedigreeFunction	= st.text_input('DiabetesPedigreeFunction value')
+    Age = st.text_input('Age')
+
+    diagnosis = ''
+    if st.button('Diabetic Test Result'):
+      diagonsis = glyco_predict([Pregnancies,Glucose,BloodPressure,SkinThickness,Insulin,BMI,DiabetesPedigreeFunction,Age])
+  
+    st.success(diagnosis)
+
+    if __name__= '__main__':
+      main()
