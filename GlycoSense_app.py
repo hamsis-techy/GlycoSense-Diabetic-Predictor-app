875d3feb98e14a669b7334ae5710c6bb738ce225
hamsis-techy <hamdaansayyed007@gmail.com>
2026-06-20 03:53:38 +0300

    Update GlycoSense_app.py

--- GlycoSense_app.py
+++ GlycoSense_app.py
@@ -22,10 +22,10 @@ def glyco_predict(inputs):
     return('The person is diabetic')
 
 def main():
-    st.header('GlycoSense - Diabeties Predictor')
-    df = pd.read_csv('diabetes.csv')
+	st.header('GlycoSense - Diabeties Predictor')
+	df = pd.read_csv('diabetes.csv')
 	st.dataframe(df.head())
-				 
+					 
     Pregnancies    = st.text_input('Number of Pregnancies')
     Glucose	       = st.text_input('Glucose level')
     BloodPressure	 = st.text_input('BP level')
