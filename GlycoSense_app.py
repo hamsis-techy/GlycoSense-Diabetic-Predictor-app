529cc7e3a18ee0e4c69792ef0ac01c4d80a2e579
hamsis-techy <hamdaansayyed007@gmail.com>
2026-06-20 04:00:29 +0300

    Update GlycoSense_app.py

--- GlycoSense_app.py
+++ GlycoSense_app.py
@@ -3,7 +3,6 @@ import numpy as np
 import pandas as pd
 import pickle as pkl
 
-
 # load the GlycoSense Model
 with open('GlycoSense_model.pkl', 'rb') as f:
     Glycosense_model = pkl.load(f)
@@ -25,12 +24,12 @@ def main():
 	st.header('GlycoSense - Diabeties Predictor')
 	df = pd.read_csv('diabetes.csv')
 	st.dataframe(df.head())
-	Pregnancies    = st.text_input('Number of Pregnancies')
-    Glucose	       = st.text_input('Glucose level')
-    BloodPressure	 = st.text_input('BP level')
-    SkinThickness	 = st.text_input('SkinThickness value')
+	Pregnancies     = st.text_input('Number of Pregnancies')
+    Glucose	        = st.text_input('Glucose level')
+    BloodPressure	= st.text_input('BP level')
+    SkinThickness	= st.text_input('SkinThickness value')
     Insulin       	= st.text_input('Insulin level')
-    BMI	           = st.text_input('BMI')
+    BMI	            = st.text_input('BMI')
     DiabetesPedigreeFunction	= st.text_input('DiabetesPedigreeFunction value')
     Age = st.text_input('Age')
 
